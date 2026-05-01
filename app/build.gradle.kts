d66c168514805d1681741e4e91262c03088e9f0b
thelegendengineer24-crypto <thelegendengineer24@gmail.com>
2026-05-01 18:23:02 +0530

    Initial commit - QueueLess UI changes in userdashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     // Material Design
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.11.0")
 
     // Testing
     testImplementation("junit:junit:4.13.2")
